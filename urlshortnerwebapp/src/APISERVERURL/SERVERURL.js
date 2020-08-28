b41a66bb98f6525fb74b6dbe141679bbb0df2f56
srihari boda <sandyhari333@gmail.com>
2020-08-28 23:02:31 +0530

    Server-url-nodified-to-hosted

--- urlshortnerwebapp/src/APISERVERURL/SERVERURL.js
+++ urlshortnerwebapp/src/APISERVERURL/SERVERURL.js
@@ -1,1 +1,2 @@
-export const SERVER_URL = "https://sw9wr.sse.codesandbox.io";
+//export const SERVER_URL = "https://sw9wr.sse.codesandbox.io"; //dev_env
+export const SERVER_URL = "https://urlshortnerapibackend.herokuapp.com"
