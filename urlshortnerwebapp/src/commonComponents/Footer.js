bd4f94dc65224ce1415bb47bab67e35a5e11ccea
srihari boda <sandyhari333@gmail.com>
2020-08-28 23:41:01 +0530

    footer changed

--- urlshortnerwebapp/src/commonComponents/Footer.js
+++ urlshortnerwebapp/src/commonComponents/Footer.js
@@ -5,8 +5,8 @@ const Footersection = () => {
     return(
     <div >
         <blockquote className="blockquote text-center">
-            <p className="mb-0 font-weight-bold">Custom URL shortening Web application.</p>
-            <footer className="blockquote-footer">Reserved  
+            <p className="mb-0 font-weight-bold">Uses of URL shortening are to "beautify" a link, track clicks, or disguise the underlying address</p>
+            <footer className="blockquote-footer">Wikipedia  
                 <cite title="Source Title"></cite>
             </footer>
         </blockquote>
